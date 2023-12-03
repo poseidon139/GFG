472392ed16ef1325fbeba42b32f796cf70360725
Surya <surya.vardhan@siemens.com>
2023-12-03 11:55:39 +0530

    first repeated character

--- Strings/find_repeated_charac.js
+++ Strings/find_repeated_charac.js
@@ -1,6 +1,27 @@
-//to find out  first characters are repeated in the given string and if no character is repeated tthen return -1 
+//to find out  first character which is repeated in the given string and if no character is repeated tthen return -1 
 
-firstRep(s){
+function firstRep(s){
     //code here
-    
+    const map = new Map();
+    for ( let i of s){
+        if (map.has(i)){
+            let count=map.get(i);
+            count++;
+            map.set(i,count);
+        }
+        else{
+            map.set(i,1);
+        }
+    }
+    let res='-1';
+    for (let i of s){
+        if (map.get(i)>1){
+            res=i;
+            break;
+        }
+    }
+    return res;
  }
+
+
+ console.log(firstRep("ge"))
