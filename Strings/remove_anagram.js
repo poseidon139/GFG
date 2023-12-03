54c7bd618d6bdbd6e2107aaa7023ecfd85020104
poseidon139 <surya03111997@gmail.com>
2023-12-03 19:41:47 +0530

    updated readme

--- Strings/remove_anagram.js
+++ Strings/remove_anagram.js
@@ -2,24 +2,7 @@
 
 function remAnagram(str1, str2){
     // code here
-    let l1=str1.length;
-    let l2=str2.length;
-    let count=0;
-    if (l1<=l2){
-        for (let i of str1){
-            if(!(str2.includes(i))){
-                count++;
-            } 
-        }
-    }
-    else{
-        for (let i of str2){
-            if(!(str1.includes(i))){
-                count++;
-            } 
-        }
-    }
-    return count;
+   
 }
 
 
