2b12b98c92ca9e478b1773d94412f094e44a2b13
poseidon139 <surya03111997@gmail.com>
2024-01-03 01:00:53 +0530

    aaded mathematics folder

---
+++ POTD/sub_array_sum0.js
@@ -0,0 +1,12 @@
+//check if the array consists of elemnsts whoose sumn can be equivalent to 0 or not 
+function nsubArrayExists(arr, n){
+    // code here
+
+    for ( let i =0 ; i< n ; i++){
+        while ( ){
+            
+        }
+    }
+    
+}
+
