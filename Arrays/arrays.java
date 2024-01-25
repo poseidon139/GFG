3dfdc7f8b04bafe3a7a78011d0105de95d2936be
poseidon139 <surya03111997@gmail.com>
2024-01-25 10:22:04 +0530

    added basic arrays definations

---
+++ Arrays/arrays.java
@@ -0,0 +1,11 @@
+package GFG.Arrays;
+
+class arrays{
+  public static void main(String[] args) {
+    int[] arr = new int[10];
+    for( int i =0 ; i < 10 ; i++){
+         arr[i]=i;
+    } 
+    System.out.println(arr);
+  }
+}
