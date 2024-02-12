97cfe5a1e303def6b73681c03459a5972187ced0
poseidon139 <surya03111997@gmail.com>
2024-02-12 15:32:10 +0530

    added comments

--- Sorting /countOnes.java
+++ Sorting /countOnes.java
@@ -14,9 +14,15 @@ public class countOnes {
 
         while( low <= high){
             int mid= (low+high)/2;
+            // if mid=0 first 1 is in the second half or right side so 
+            // we do low= mid +1
             if(arr[mid]==0){
                 low= mid+1;
             }
+            // if it is 1 then two case arises either it is first one and any one in the midddle 
+            // for first one whoose index is 0 mid =0 and if it first one in between them previous element should be zero 
+            // which is arr[mid-1]=0
+            //else if it not the first one we update high=mid -1 and again use binary search to find first one on left side
             else{
                 if( mid==0 || arr[mid-1]==0){
                     return  (n-mid);
