929985cd830f5a1006c9e538c3b1d855ac08e15e
poseidon139 <surya03111997@gmail.com>
2024-02-13 12:08:49 +0530

    added new sols

--- Searching/InfiniteSizeArrays.java
+++ Searching/InfiniteSizeArrays.java
@@ -10,19 +10,19 @@ public class InfiniteSizeArrays {
 
     /// naive apporch/////////
 
-    public static int InfiniteSizeArraysFunc(int arr[], int x ){
-        int pos=-1; 
-        for ( int i =0 ; i < arr.length; i++){
-            if( arr[i]==x){
-                pos=i; 
-                break;
-            }
-            else if(arr[i]<x && arr[i+1]>x){
-                pos=-1;
-            }
-        }
-        return pos;
-    }
+    // public static int InfiniteSizeArraysFunc(int arr[], int x ){
+    //     int pos=-1; 
+    //     for ( int i =0 ; i < arr.length; i++){
+    //         if( arr[i]==x){
+    //             pos=i; 
+    //             break;
+    //         }
+    //         else if(arr[i]<x && arr[i+1]>x){
+    //             pos=-1;
+    //         }
+    //     }
+    //     return pos;
+    // }
 
 
     // ///Efficient Approach
@@ -31,10 +31,28 @@ public class InfiniteSizeArrays {
     //     return -1;
     // }
 
-
-
-
-
+    public static int InfiniteSizeArraysFunc(int[] arr, int x){
+        int pos=-1;
+        int low=0; 
+        int high= 1;
+        if (x==arr[0]){
+            return 0;
+        }
+        while (low<high){
+            int mid= (low+high)/2;
+            if (arr[mid]==x){
+                pos=mid;
+            }
+            else if (x<arr[mid]){
+                high= mid-1;
+            }
+            else{
+                low= mid+1;
+                high=high*2;
+            }
+        }
+        return pos;
+    }
 
 
     public static void main(String[] args) {
