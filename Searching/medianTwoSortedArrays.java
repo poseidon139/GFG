791c5ac120c953971a4ae472dd292514326cbc6d
poseidon139 <surya03111997@gmail.com>
2024-02-20 21:13:15 +0530

    added more sols

---
+++ Searching/medianTwoSortedArrays.java
@@ -0,0 +1,13 @@
+// naive solution 
+
+
+public class medianTwoSortedArrays {
+    public int medianTwoSortedArraysFunc(int[] arr2, int[] arr2){
+        
+    }
+    public static void main(String[] args) {
+        int arr1[]={10,20,30,40};
+        int arr2[]={50,60,70,80};
+        
+    }
+}
