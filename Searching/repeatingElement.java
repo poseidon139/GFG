eee170afe863d0fe1b64d701c5c5c3e68b206f18
poseidon139 <surya03111997@gmail.com>
2024-02-29 13:42:43 +0530

    added new solutions

---
+++ Searching/repeatingElement.java
@@ -0,0 +1,15 @@
+//conditions in repeating arrays 
+// array  where all element appears exactly once except one 
+// and all the element from 0 to max is present.
+{0,1,2,3,3,3,4,6,5,7}
+
+
+public class repeatingElement {
+    public static int repeatingElementFunc(int arr[]){
+        
+    }
+    public static void main(String[] args) {
+        int[] arr={0,0,22,32,03,02,03,01,23};
+        System.out.println(repeatingElementFunc(arr));
+    }
+}
