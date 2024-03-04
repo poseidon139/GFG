f2efd8f7b75c2bbdd5627e860761b1602c2a1220
poseidon139 <surya03111997@gmail.com>
2024-03-04 21:40:26 +0530

    added files

--- Searching/repeatingElement.java
+++ Searching/repeatingElement.java
@@ -1,7 +1,7 @@
 //conditions in repeating arrays 
 // array  where all element appears exactly once except one 
 // and all the element from 0 to max is present.
-{0,1,2,3,3,3,4,6,5,7}
+
 
 
 public class repeatingElement {
@@ -9,7 +9,6 @@ public class repeatingElement {
         
     }
     public static void main(String[] args) {
-        int[] arr={0,0,22,32,03,02,03,01,23};
-        System.out.println(repeatingElementFunc(arr));
+        
     }
 }
---
+++ Sorting/simpSort.java
@@ -0,0 +1,12 @@
+package Sorting;
+
+public class simpSort {
+
+    public static void simpSortFunc(int[] arr){
+
+    }
+    public static void main(String[] args) {
+        int[] arr={2,1,4,3,6,5,9};
+        System.out.println(arr);
+    }
+}
