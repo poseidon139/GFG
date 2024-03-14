24768ab595c3603052504fab72e7aef2188e1219
poseidon139 <surya03111997@gmail.com>
2024-03-14 08:41:10 +0530

    added new files

--- Sorting/stabilityInSorting.java
+++ Sorting/stabilityInSorting.java
@@ -1,6 +1,6 @@
 package Sorting;
 
-// as orting algo is stable if the  relative order of equal sort elements is preserved. 
+// as sorting algo is stable if the  relative order of equal sort elements is preserved. 
 
 public class stabilityInSorting {
     
